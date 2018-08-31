d2a0319d0715b48178b82af329e24e1ba5f88bb0
lohita <lohita@192.168.2.21>
2018-08-31 17:08:29 -0400

    Created the new class Welcome

---
+++ Bank7_Project/src/test/java/com/Bank/TestCases/WelcomeTest.java
@@ -0,0 +1,10 @@
+package com.Bank.TestCases;
+
+public class WelcomeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome");
+	}
+
+}
